9c061c7302cea7990d40ac150e029e37520ae95f
weiwei <2322246386@qq.com>
2021-11-24 21:33:44 +0800

    页面尾部制作

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -11,20 +11,26 @@
     <router-view />
   </div>
   <!-- 底部组件 -->
-  <footer>底部组件</footer>
+  <Footer />
+  <!-- <footer>底部组件</footer> -->
 
   <button @click="$store.commit('user/setUserInfo', {token: 3344, nickname: 'weiwei3344'})">登录</button>
   <button @click="$store.commit('user/setUserInfo', {})">退出</button>
 </template>
 
 <script>
+// 引入顶部菜单栏
 import NavBar from '@/components/app-navbar.vue'
+// 引入头部导航栏
 import Header from '@/components/app-header.vue'
+// 引入尾部状态栏
+import Footer from '@/components/app-footer.vue'
 export default {
   name: 'Layout',
   components: {
     NavBar,
-    Header
+    Header,
+    Footer
   }
 
   // setup () {
