230dc43a94a9c6be403cc1a39f031f8ee073bdfc
weiwei <2322246386@qq.com>
2021-12-02 20:58:00 +0800

    弹层展示

--- src/views/home/components/home-category.vue
+++ src/views/home/components/home-category.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-category">
     <ul class="menu">
-      <li v-for="item in menuList" :key="item.id">
+      <li v-for="item in menuList" :key="item.id" @mouseenter="categoryId = item.id">
         <RouterLink :to="`/category/${item.id}`">{{item.name}}</RouterLink>
         <!-- 没有数据的时候不显示 -->
         <template v-if="item.children">
@@ -13,11 +13,27 @@
         </template>
       </li>
     </ul>
+    <!-- 弹层，弹出商品 -->
+    <div class="layer">
+      <h4>分类推荐<small>根据您的购买或者浏览记录推荐</small></h4>
+      <ul v-if="currCategory && currCategory.goods">
+        <li v-for="item in currCategory.goods" :key="item.id">
+          <RouterLink to="/">
+            <img :src="item.picture" alt="">
+            <div class="info">
+              <p class="name ellipsis-2">{{item.name}}</p>
+              <p class="dessc ellipsis">{{item.desc}}</p>
+              <p class="price"><i>￥</i>{{item.price}}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, computed } from 'vue'
+import { reactive, computed, ref } from 'vue'
 import { useStore } from 'vuex'
 export default {
   name: 'HomeCategory',
@@ -33,7 +49,8 @@ export default {
       name: '品牌',
       children: [
         {
-          id: 'brand-children', name: '品牌推荐'
+          id: 'brand-children',
+          name: '品牌推荐'
         }
       ]
     })
@@ -47,14 +64,25 @@ export default {
           id: item.id,
           name: item.name,
           // 防止初始化的时候children中没有数据调用slice函数报错
-          children: item.children && item.children.slice(0, 2)
+          children: item.children && item.children.slice(0, 2),
+          goods: item.goods
         }
       })
       list.push(brand)
       return list
     })
+
+    // 获取当前分类下的数据
+    // 需要定义一个获取当前商品的id，再去数据中遍历寻找，如果找到，就赋值下面创建的数据中，在返回出去给模板使用
+    const categoryId = ref(null)
+    const currCategory = computed(() => {
+      return menuList.value.find(item => item.id === categoryId.value)
+    })
+    setTimeout(() => console.log(menuList.value, currCategory.value), 3000)
     return {
-      menuList
+      menuList,
+      categoryId,
+      currCategory
     }
   }
 }
@@ -81,6 +109,80 @@ export default {
         color: #fff;
         &:first-child{
           font-size: 16px;
+        }
+      }
+    }
+  }
+  &:hover{
+    .layer{
+      display: block;
+    }
+  }
+  .layer{
+    width: 990px;
+    height: 500px;
+    background: rgba(255, 255, 255, 0.8);
+    position: absolute;
+    left: 250px;
+    top: 0;
+    display: none;
+    padding: 0 15px;
+    h4{
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+      small{
+        font-size: 16px;
+        color: #666;
+      }
+    }
+    ul{
+      display: flex;
+      flex-wrap: wrap;
+      li{
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        background: #fff;
+        // nth-children选择器匹配属于父元素的第N个子元素，无论元素的类型
+        &:nth-child(3n){
+          margin-right: 0;
+        }
+        a{
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          padding: 10px;
+          &:hover{
+            background: #e3f9f4;
+          }
+          img{
+            width: 95px;
+            height: 95px;
+          }
+          .info{
+            padding-left: 10px;
+            line-height: 24px;
+            width: 190px;
+            .name{
+              font-size: 16px;
+              color: #666;
+            }
+            .desc{
+              color:#999;
+            }
+            .price{
+              font-size: 22px;
+              color: @weiweiColor;
+              i{
+                font-size: 16px;
+              }
+            }
+          }
         }
       }
     }
