49866950eff585b5e6da5fecac3d8d664c9d10c6
weiwei <2322246386@qq.com>
2021-12-15 21:24:20 +0800

    未完成的商品区块

---
+++ src/views/home/components/home-product.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home-product">
+    <HomePanel title="生鲜" v-for="i in 4" :key="i">
+      <template #right>
+        <div class="sub">
+          <RouterLink to="/">海鲜</RouterLink>  
+          <RouterLink to="/">水果</RouterLink>  
+          <RouterLink to="/">蔬菜</RouterLink>  
+          <RouterLink to="/">水产</RouterLink>  
+        </div>
+        <WeiweiMore />
+      </template>
+      <div class="box">
+        <RouterLink class="cover" to="/">
+          <img src="http://zhoushugang.gitee.io/erabbit-client-pc-static/uploads/fresh_goods_cover.jpg" alt="">
+          <strong class="label">
+            <span>生鲜馆</span>
+            <span>全场3件7</span>
+          </strong>
+        </RouterLink>
+      </div>
+    </HomePanel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeProduct'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
