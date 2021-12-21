08ba7a4e2322e01ea9bfc1c3257e26e0eceffd41
weiwei <2322246386@qq.com>
2021-12-21 20:10:39 +0800

    面包屑高级用法，还是有点模糊

--- src/components/library/weiwei-bread-item.vue
+++ src/components/library/weiwei-bread-item.vue
@@ -2,7 +2,7 @@
   <div class="weiwei-bread-item">
     <RouterLink v-if="to" :to="to"><slot /></RouterLink>
     <span v-else><slot /></span>
-    <i class="iconfont icon-angle-right"></i>
+    <!-- <i class="iconfont icon-angle-right"></i> -->
   </div>
 </template>
 
--- src/components/library/weiwei-bread.vue
+++ src/components/library/weiwei-bread.vue
@@ -1,12 +1,33 @@
-<template>
-  <div class="weiwei-bread">
-    <slot />
-  </div>
-</template>
 
 <script>
+
+// 引入h函数
+import { h } from 'vue'
+
 export default {
-  name: 'WeiweiBread'
+  name: 'WeiweiBread',
+  render () {
+    // 用法
+    // 1. template标签去掉，单文件组件
+    // 2. 返回值就是组件内容
+    // 3. vue2.0 的h函数传参进来的，vue3.0的h函数导入进来的
+    // 4. h第一个参数标签名字 第二个参数 标签属性对象 第三个参数 子节点数
+    // 需求
+    // 1. 创建xtx-braad芙蓉国企
+    // 2. 获取默认插槽内容
+    // 3. 去除xtx-bread-item组件的标签,由render函数来组织
+    // 4. 遍历插槽中的item,得到一个动态创建的节点,最后一个item不加标签
+    // 5. 把动态创建的节点渲染在xtx-bread标签中
+    const items = this.$slots.default()
+    const dymanicItems = []
+    items.forEach((element, index) => {
+      dymanicItems.push(element)
+      if (index < (items.length - 1)) {
+        dymanicItems.push(h('i', { class: 'iconfont icon-angle-right' }))
+      }
+    })
+    return h('div', { class: 'weiwei-bread' }, dymanicItems)
+  }
 }
 </script>
 
